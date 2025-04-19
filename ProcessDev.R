fa4c242f7902c27532041e4fe36b9069ef551956
Surfinokie <81388027+Surfinokie@users.noreply.github.com>
2025-04-19 07:29:45 -1000

    Final working proof of concept. Code cleanup, debug code removal, and make main runner a function.

--- ProcessDev.R
+++ ProcessDev.R
@@ -2,31 +2,30 @@
 #R version 4.4.3
 #Tidyverse 2.0.0 (dplyr 1.1.4)
 library(tidyverse)
-#library(plyr)
 
 #Read in the data
 #windows
 #baseframe <- read.csv("c:\\work\\R\\BinGenerator\\Data\\pr_usvi1623_fish18sppLH_depregdat.csv", header = TRUE)
+#baseframe <- read.csv("D:\\work\\Analyses\\SAS_to_R\\pr_usvi1623_fish18sppLH_depregdat.csv", header = TRUE)
 #mac
 baseframe <- read.csv("../pr_usvi1623_fish18sppLH_depregdat.csv", header = TRUE)
-
-#baseframe
-#summary(baseframe)
 
 #create a new variable for depth category (DC) in a new dataframe
 #yeah, I know we don't need a new dataframe but I like to be able to debug easily
 ana_frame <-
   baseframe %>%
-  mutate(dc = floor(DEPTH))
+    mutate(dc = floor(DEPTH))
 
-ana_frame <- ana_frame %>% mutate(species_cd = str_replace_all(species_cd, " ", "__"))
+#ana_frame <- ana_frame %>% mutate(species_cd = str_replace_all(species_cd, " ", "__"))
 
 #Create new, currently empty columns for the min, max, and midpoint depth categories
 ana_frame <- ana_frame %>%
   add_column("dc_min"=0, "dc_max"=0, "dc_midpoint"=0)
 
 #vector of spp codes
-distinct_spp <- ana_frame %>% distinct(species_cd) %>% pull()
+distinct_spp <- ana_frame %>% 
+                  distinct(species_cd) %>% 
+                    pull()
 
 result <- data.frame()
 final_result <- data.frame()
@@ -34,8 +33,8 @@ the_frame <- data.frame()
 #get all rows with the species code and create a new summarised df on which to operate
 spp_extractor <- function(spp_code){
   filter(ana_frame, species_cd == spp_code) %>%
-    group_by(species_cd, dc) %>%
-    summarise(n=n(), 
+            group_by(species_cd, dc) %>%
+              summarise(n=n(), 
               obs=max(pres), 
               pres_true = sum(ifelse(pres==1, 1, 0)), 
               pres_false = sum(ifelse(pres==0, 1, 0)))
@@ -67,17 +66,7 @@ process_spp <- function(processing_frame, threshold){
       vNeg <- append(vNeg, neg_sum)
       vSppCd <- append(vSppCd, processing_frame[i,1])
       vDc <- append(vDc, processing_frame[i,2])
-      
-      #debug code
-      #print(vObs)
-      #print(vSppCd)
-      
       result_frame <- data.frame(unlist(vSppCd), vObs, min(unlist(vDc)), max(unlist(vDc)), vPos, vNeg)
-      
-      #debug code
-      #print(result_frame)
-      #if(result_frame[1,3] == result_frame[1,4]){
-      #  print("true")
       
       #set the current row's max_dc to the next row's min_dc
       if(!is.na(processing_frame[i+1, 2]))
@@ -87,29 +76,23 @@ process_spp <- function(processing_frame, threshold){
       {
         result_frame[1,4] = processing_frame[i,2]
       }
-      
-      #debug code
-      #print(result_frame)
-      
+
       rows_to_add <- append(rows_to_add, result_frame)
-      #print(rows_to_add)
       i <- i + 1
     }
-    
-    #debug code  
-    #print("result")
-    #print(result_frame)
-    #print("end result")
-    
+
     colnames(result_frame) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
-    intermed_result_frame <- result_frame %>% group_by(species_cd) %>% summarise(max(obs), min(min_dc), min(min_dc), max(max_dc), sum(pos_obs), sum(neg_obs))
+    intermed_result_frame <- result_frame %>% group_by(species_cd) %>% 
+                                summarise(max(obs), 
+                                          min(min_dc), 
+                                          min(min_dc), 
+                                          max(max_dc), 
+                                          sum(pos_obs), 
+                                          sum(neg_obs))
     colnames(intermed_result_frame) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
-    #debug code
-    #print(intermed_result_frame)
-    
+
     result <- rbind(result, intermed_result_frame)
-    #colnames(result) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
-    #print(result)
+
   }
   
   #If the last row is < the threshold, add it to the preceding row, and remove the last
@@ -118,7 +101,7 @@ process_spp <- function(processing_frame, threshold){
     result[nrow(result)-1, 4] <- result[nrow(result), 4]
     result[nrow(result)-1, 5] <- result[nrow(result)-1, 5] + result[nrow(result), 5]
     result[nrow(result)-1, 6] <- result[nrow(result)-1, 6] + result[nrow(result), 6]
-
+    
     result <- result %>% filter(!row_number() %in% nrow(result))
   }
   
@@ -128,9 +111,8 @@ process_spp <- function(processing_frame, threshold){
 }
 
 process_logit <- function(logit_frame){
-    #go through result frame and add subsequent rows until pos_obs and neg_obs both >= 1
-    #do pos_obs first
-  print(logit_frame)
+  #go through result frame and add subsequent rows until pos_obs and neg_obs both >= 1
+  #do pos_obs first
   vCols <<- c("pos_obs", "neg_obs")
   
   for(j in seq_along(vCols)){
@@ -144,12 +126,8 @@ process_logit <- function(logit_frame){
     vPosObs <- vector()
     vNegObs <- vector()
     intermed_result_frame <- data.frame(species_cd=character(), obs=numeric(), min_dc=numeric(), max_dc=numeric(), pos_obs=numeric(), neg_obs=numeric())
-    #colnames(intermed_result_frame) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
-    print("yeah")
     #this loop takes care of everything except potentially the last row
     while(sit<=nrow(logit_frame)){
-    #while(sit<8){
-      #print(current_sum)
       while(current_sum < 1 && sit<=nrow(logit_frame)){
         current_sum <- current_sum + sum(logit_frame[sit, vCols[j]])
         vSppCd <- append(vSppCd, logit_frame[sit,c("species_cd")])
@@ -158,50 +136,14 @@ process_logit <- function(logit_frame){
         vMaxDc <- append(vMaxDc, logit_frame[sit, c("max_dc")])
         vPosObs <- append(vPosObs, logit_frame[sit, c("pos_obs")])
         vNegObs <- append(vNegObs, logit_frame[sit, c("neg_obs")])
-        
-        # print(paste("sum:", current_sum))
-        # print(vSppCd)
-        # print(vObs)
-        # print(vMinDc)
-        # print(vMaxDc)
-        # print(vPosObs)
-        # print(vNegObs)
-        #print(length(vObs))
-        #rbind here instead
-        #print(intermed_result_frame)
-        #print(data.frame(vSppCd, vObs, vMinDc, vMaxDc, vPosObs, vNegObs))
-        #tempdf <- data.frame(unlist(vSppCd), unlist(vObs), unlist(vMinDc), unlist(vMaxDc), unlist(vPosObs), unlist(vNegObs))
-        #intermed_result_frame <- rbind(intermed_result_frame, tempdf)
-        #print(str(intermed_result_frame))
-        #intermed_result_frame <- data.frame(vSppCd, vObs, vMinDc, vMaxDc, vPosObs, vNegObs)
-        #print(str(intermed_result_frame))
         sit<-sit+1
-        #print(intermed_result_frame)
-        #print(paste("inner:", current_sum))
       }
-      # print(paste("sum:", current_sum))
-      # print(vSppCd)
-      # print(vObs)
-      # print(vMinDc)
-      # print(vMaxDc)
-      # print(vPosObs)
-      # print(vNegObs)
-      # print(length(current_sum))
-      # print(length(vSppCd))
-      # print(length(vObs))
-      # print(length(vMinDc))
-      # print(length(vMaxDc))
-      #print(intermed_result_frame)
-      #we can lose this if we change the group by columns
-      #colnames(intermed_result_frame) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
       tempdf <- data.frame(unlist(vSppCd), unlist(vObs), unlist(vMinDc), unlist(vMaxDc), unlist(vPosObs), unlist(vNegObs))
-      print(tempdf)
       colnames(tempdf) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
       result_frame <- tempdf %>% group_by(species_cd) %>% summarise(sum(obs), min(min_dc), max(max_dc), sum(pos_obs), sum(neg_obs))
       colnames(result_frame) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
-      #print(result_frame)
       result <- rbind(result, result_frame)
-      #print(result)
+      
       #clear all the variables for next loop
       intermed_result_frame <- intermed_result_frame[0,]
       result_frame <- result_frame[0,]
@@ -213,19 +155,11 @@ process_logit <- function(logit_frame){
       vPosObs <- vector()
       vNegObs <- vector()
       current_sum <- 0
-      #print(sit)
     }
     
     colnames(result) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
-    #print(j)
-    #print(vCols[j])
-    #handle the last row case
-    #print(result)
-    #print(result[nrow(result), vCols[j]])
-    
-    #need a recursive call here
     if(result[nrow(result), vCols[j]] == 0){
-      print("in last row")
+      logit_frame<-result
       result[nrow(result)-1, c("obs")] <- result[nrow(result)-1, c("obs")] + result[nrow(result), c("obs")]
       result[nrow(result)-1, c("max_dc")] <- result[nrow(result), c("max_dc")]
       result[nrow(result)-1, vCols[j]] <- result[nrow(result)-1, vCols[j]]
@@ -234,58 +168,63 @@ process_logit <- function(logit_frame){
     }
     logit_frame<-result
   }  
-#print(logit_frame)
+  
+  #really need to turn this into a function probably called by the function caller
+  notcol <- vCols[!(vCols %in% vCols[j])]
+  if((logit_frame[nrow(logit_frame), vCols[j]] == 0) || (logit_frame[nrow(logit_frame), vCols[!(vCols %in% vCols[j])]] == 0)){
+    logit_frame[nrow(logit_frame)-1, c("obs")] <- logit_frame[nrow(logit_frame)-1, c("obs")] + logit_frame[nrow(logit_frame), c("obs")]
+    logit_frame[nrow(logit_frame)-1, c("max_dc")] <- logit_frame[nrow(logit_frame), c("max_dc")]
+    logit_frame[nrow(logit_frame)-1, vCols[j]] <- logit_frame[nrow(logit_frame)-1, vCols[j]]
+    logit_frame[nrow(logit_frame)-1, vCols[vCols != vCols[j]]] <- logit_frame[nrow(logit_frame)-1, vCols[vCols != vCols[j]]] + logit_frame[nrow(logit_frame), vCols[vCols != vCols[j]]]
+    logit_frame <- logit_frame %>% filter(!row_number() %in% nrow(logit_frame))
+  }
   return(logit_frame)
 }
 
 #for each element in distinct_spp call the function that does all the work and add the result to the final result df
-if(length(distinct_spp) > 0){
-  for(i in distinct_spp){
-    #i == the species_cd
-    #call the function
-    the_frame <- spp_extractor(i)
-    if(which(distinct_spp == i) > 1){
-      final_result <- union_all(final_result, process_spp(the_frame, 40))
-    }
-    else {
-      final_result <- process_spp(the_frame, 40)
-    }
-  }
-  
-  colnames(final_result) <- c("species_cd", "obs", "min_dc", "max_dc", "pos_obs", "neg_obs")
-  #print(str())
-  logit <- TRUE
-  #logistic portion
-  #print(distinct_spp)
-  if(logit == TRUE){
-    if(length(distinct_spp > 0)){
-      for(s in distinct_spp){
-        #print(s)
-        #the_frame <-filter(final_result, species_cd == s)
-        the_frame <-filter(final_result, species_cd == "BAL__CAPR")
-        as.data.frame(the_frame)
-        #print(str(the_frame))
-         if(which(distinct_spp == s) > 1){
-           print("here")
-           #print(the_frame)
-           im_final_result <- process_logit(the_frame)
-           #final_result <- union_all(final_result, process_logit(the_frame))
-        #
-         } else {
-           print("there")
-           #print(the_frame)
-          #final_result <- process_logit(the_frame)
-        }
+generate_bins <- function(bin_size = 25, logit = FALSE){
+  if(length(distinct_spp) > 0){
+    for(i in distinct_spp){
+      #i == the species_cd
+      #call the function
+      the_frame <- spp_extractor(i)
+      if(which(distinct_spp == i) > 1){
+        final_result <- union_all(final_result, process_spp(the_frame, bin_size))
+      }
+      else {
+        final_result <- process_spp(the_frame, bin_size)
       }
     }
-  }
+    
+    ir <- final_result[0,]
 
-  #print(final_result)
-  #Add a final midpoint column that is the mid between min_dc and max_dc
-  # final_result <-
-  #   final_result %>%
-  #   mutate(midpoint = (min_dc + max_dc)/2)
-} #what to do if not true?
+    #logistic portion
+    if(logit == TRUE){
+      if(length(distinct_spp > 0)){
+        for(s in distinct_spp){
+          the_frame <-filter(final_result, species_cd == s)
+          as.data.frame(the_frame)
+          
+          ir <- union_all(ir, as.data.frame(process_logit(the_frame)))
+        }
+      }
+      final_result <- ir
+    }
+    
+    #Add a final midpoint column that is the mid between min_dc and max_dc
+    final_result <-
+      final_result %>%
+        mutate(midpoint = (min_dc + max_dc)/2)
+    
+    return(final_result)
+  } #what to do if not true?
+}
 
+#different ways to run this
+#my_result <- generate_bins(25)
+#my_result <- generate_bins(25, FALSE)
+#my_result <- generate_bins(40, TRUE)
+#my_result <- generate_bins(bin_size = 25)
+#my_result <- generate_bins(bin_size = 40, logit=TRUE)
 
-write.csv(final_result, "../logit_test_data.csv")
+#write.csv(my_result, "../logit_test_data.csv")
